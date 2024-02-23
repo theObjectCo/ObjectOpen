c55224b573bde0fc2c2d56ce6a07b7b9411c137e
Mateusz Zwierzycki <mateusz@object.pl>
2024-02-23 20:33:00 +0100

    separating stateless

--- ObjectOpen/ObjectOpen.Patterns/Solvers/Solver.cs
+++ ObjectOpen/ObjectOpen.Patterns/Solvers/Solver.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace ObjectOpen.Patterns.Solvers
 {
-
     public abstract class Solver
     {
+        public string Name { get; set; }
+
         public Solver() { }
 
-        public string Name { get; set; }
         protected Solver(string name)
         {
             Name = name;
@@ -69,51 +69,6 @@ namespace ObjectOpen.Patterns.Solvers
 
             if (Inputs != null) data.Add("Inputs", Inputs);
             if (Outputs != null) data.Add("Outputs", Outputs);
-
-            var sets = new Newtonsoft.Json.JsonSerializerSettings();
-            sets.Formatting = Newtonsoft.Json.Formatting.Indented;
-
-            return Newtonsoft.Json.JsonConvert.SerializeObject(data, sets);
-        }
-    }
-
-    public abstract class StatelessSolver<TInputs, TOutputs> : Solver
-      where TInputs : SolverInputs
-      where TOutputs : SolverOutputs
-    {
-        public StatelessSolver() : base() { }
-        public StatelessSolver(string name = "") : base(name) { }
-        public abstract Result<TOutputs> SolveInternal(TInputs inputs);
-        public virtual Result<TOutputs> Solve(TInputs inputs)
-        {
-            Result<TOutputs> res;
-
-            try
-            {
-                res = SolveInternal(inputs);
-            }
-            catch (Exception ex)
-            {
-                res = new Result<TOutputs>(Flag.Error, ex.Message);
-            }
-
-            return res;
-        }
-        public override Type GetInputsType()
-        {
-            return typeof(TInputs);
-        }
-        public override Type GetOutputsType()
-        {
-            return typeof(TOutputs);
-        }
-        public override string ToJSON()
-        {
-            SortedList<string, object> data = new SortedList<string, object>
-            {
-                { "SolverType", GetType().FullName },
-                { "Assembly", GetType().Assembly.FullName }
-            };
 
             var sets = new Newtonsoft.Json.JsonSerializerSettings();
             sets.Formatting = Newtonsoft.Json.Formatting.Indented;
---
+++ ObjectOpen/ObjectOpen.Patterns/Solvers/StatelessSolver.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectOpen.Patterns.Solvers
+{
+    public abstract class StatelessSolver
+    {
+        public string Name { get; set; }
+
+        public StatelessSolver() { }
+
+        protected StatelessSolver(string name)
+        {
+            Name = name;
+        }
+
+        public abstract Type GetInputsType();
+        
+        public abstract Type GetOutputsType();
+
+        public virtual string ToJSON() { return ""; }
+    }
+
+    public abstract class StatelessSolver<TInputs, TOutputs> : StatelessSolver
+      where TInputs : SolverInputs
+      where TOutputs : SolverOutputs
+    {
+        public StatelessSolver() : base() { }
+        public StatelessSolver(string name = "") : base(name) { }
+        public abstract Result<TOutputs> SolveInternal(TInputs inputs);
+        public virtual Result<TOutputs> Solve(TInputs inputs)
+        {
+            Result<TOutputs> res;
+
+            try
+            {
+                res = SolveInternal(inputs);
+            }
+            catch (Exception ex)
+            {
+                res = new Result<TOutputs>(Flag.Error, ex.Message);
+            }
+
+            return res;
+        }
+        public override Type GetInputsType()
+        {
+            return typeof(TInputs);
+        }
+        public override Type GetOutputsType()
+        {
+            return typeof(TOutputs);
+        }
+        public override string ToJSON()
+        {
+            SortedList<string, object> data = new SortedList<string, object>
+            {
+                { "SolverType", GetType().FullName },
+                { "Assembly", GetType().Assembly.FullName }
+            };
+
+            var sets = new Newtonsoft.Json.JsonSerializerSettings();
+            sets.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(data, sets);
+        }
+    }
+}
