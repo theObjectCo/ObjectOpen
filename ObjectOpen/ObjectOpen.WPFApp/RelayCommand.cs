6302c3af9d600b7f22c43a9cd919117965ccc3fd
Kiryl Furmanchuk <kirill.furmanchuk@object.pl>
2024-04-10 18:34:55 +0200

    Init

---
+++ ObjectOpen/ObjectOpen.WPFApp/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace ObjectOpen.WPFApp
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object>? execute) : this(execute, null) { }
+
+        public RelayCommand(Action<object>? execute, Predicate<object>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object? parameter) =>
+            _canExecute == null || _canExecute(parameter
+                ?? throw new ArgumentNullException(nameof(parameter)));
+
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object? parameter) =>
+            _execute(parameter
+                ?? throw new ArgumentNullException(nameof(parameter)));
+    }
+}
---
+++ ObjectOpen/ObjectOpen.WPFApp/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ObjectOpen.WPFApp
+{
+    public class ViewModel : ViewModelBase
+    {
+        public ObservableCollection<Employee> Employees { get; private set; }
+
+
+    }
+}
